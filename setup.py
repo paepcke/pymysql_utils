ea72167544c050313d7197b43b8117286f510a55
Andreas Paepcke <paepcke@cs.stanford.edu>
2014-03-26 08:48:54 -0700

    Initial commit

---
+++ setup.py
@@ -0,0 +1,33 @@
+import multiprocessing
+from setuptools import setup, find_packages
+setup(
+    name = "pymysql_utils",
+    version = "0.1",
+    packages = find_packages(),
+
+    # Dependencies on other packages:
+    # Couldn't get numpy install to work without
+    # an out-of-band: sudo apt-get install python-dev
+    setup_requires   = [],
+    install_requires = ['pymysql3>=0.5', 
+			],
+    # tests_require    = ['mongomock>=1.0.1', 'sentinels>=0.0.6', 'nose>=1.0'],
+
+    # Unit tests; they are initiated via 'python setup.py test'
+    #test_suite       = 'nose.collector', 
+
+    package_data = {
+        # If any package contains *.txt or *.rst files, include them:
+     #   '': ['*.txt', '*.rst'],
+        # And include any *.msg files found in the 'hello' package, too:
+     #   'hello': ['*.msg'],
+    },
+
+    # metadata for upload to PyPI
+    author = "Andreas Paepcke",
+    author_email = "paepcke@cs.stanford.edu",
+    description = "Thin wrapper around pymysql. Provides Python iterator for queries. Abstracts away cursor.",
+    license = "BSD",
+    keywords = "MySQL",
+    url = "https://github.com/paepcke/pymysql_utils",   # project home page, if any
+)
