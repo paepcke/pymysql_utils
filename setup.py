794c5e49173a5135213aa52cdc68de995f571704
Andreas Paepcke <paepcke@cs.stanford.edu>
2017-07-02 11:22:41 -0700

    Bumped version to 1.5.

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ datafiles = ['pymysql_utils/data/ipToCountrySoftware77DotNet.csv']
 
 setup(
     name = "pymysql_utils",
-    version = "1.4",
+    version = "1.5",
     packages = find_packages(),
 
     # Dependencies on other packages:
