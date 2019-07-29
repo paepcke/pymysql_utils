0367094087924618d517606bb12c96df909514b2
Andreas Paepcke <paepcke@cs.stanford.edu>
2019-07-29 15:35:49 -0700

    Bumped version to 2.1.1

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ with open("README.md", "r") as fh:
     long_description = fh.read()
 setup(
     name = "pymysql_utils",
-    version = "2.1.0",
+    version = "2.1.1",
     packages = find_packages(),
 
     # Dependencies on other packages:
